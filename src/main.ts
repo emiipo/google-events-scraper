2a12ec73f888883b109d863b753a47cd8d1b44d9
emi <emii.povi@gmail.com>
2023-06-28 22:06:55 +0100

    added scraping for most of the data

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,39 @@ const crawler = new PlaywrightCrawler({
         //Scrape the content
         const events = await page.locator('div[jsname="qlMead"]').all();
         for(const evnt of events){
-            const name = await evnt.locator('div[jsname="r4nke"]').textContent();
-            log.info(`${name}`);
+            //Description handling just in case it doesn't have one
+            let desc = '';
+            const descLocator = await evnt.locator('.PVlUWc');
+            if(await descLocator.count() > 0){
+                desc = await descLocator.textContent();
+            }
+
+            const mapUrl = await evnt.locator('.ozQmAd').getAttribute('data-url');
+
+            //Links handling
+            let links = [];
+            const linkLocators = await evnt.locator('.SKIyM').all();
+            for (const lnk of linkLocators) {
+                const link = {
+                    name: await lnk.locator('.NLMF7b span').textContent(),
+                    url: await lnk.getAttribute('href'),
+                }
+                links.push(link);
+            }
+
+            const results = {
+                name: await evnt.locator('div[jsname="r4nke"]').textContent(),
+                description: desc,
+                date: await evnt.locator('.Gkoz3').textContent(),
+                location: {
+                    addressMain: await evnt.locator('.n3VjZe').textContent(),
+                    addressSecondary: await evnt.locator('.U6txu').textContent(),
+                    mapUrl: 'https://google.com' + mapUrl,
+                },
+                links: links,
+            }
+
+            console.log(results);
         }
     },
     headless: false,
