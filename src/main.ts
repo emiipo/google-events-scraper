f0b068e9d967c9dbfe35dc83863087797a598e1b
emi <emii.povi@gmail.com>
2023-07-01 04:02:31 +0100

    I think i fixed everything to be able to upload this as a package

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const timezones = ['ACDT', 'ACST', 'ACT', 'ACWST', 'ADT', 'AEDT', 'AEST', 'AET',
 const oneLetterTimezones = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const months = new Map<string, number>([['Jan',1], ['Feb',2], ['Mar',3], ['Apr',4], ['May',5], ['Jun',6], ['Jul',7], ['Aug',8], ['Sept',9], ['Oct', 10], ['Nov', 11], ['Dec', 12]]);
 
-class gEventsScraper {
+export default class gEventsScraper {
     data = [];
 
     async Scrape(query:string, options?:{ today?:boolean, tomorrow?:boolean, thisWeek?:boolean, thisWeekend?:boolean, nextWeek?:boolean, nextMonth?:boolean}):Promise<any> {
