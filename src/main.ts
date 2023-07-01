bea3c44d78dc5c96210168985e41e09a497eeceb
emi <emii.povi@gmail.com>
2023-07-01 03:21:31 +0100

    setup the package to correctly export the class

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const timezones = ['ACDT', 'ACST', 'ACT', 'ACWST', 'ADT', 'AEDT', 'AEST', 'AET',
 const oneLetterTimezones = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const months = new Map<string, number>([['Jan',1], ['Feb',2], ['Mar',3], ['Apr',4], ['May',5], ['Jun',6], ['Jul',7], ['Aug',8], ['Sept',9], ['Oct', 10], ['Nov', 11], ['Dec', 12]]);
 
-export default class gEventsScraper {
+class gEventsScraper {
     data = [];
 
     async Scrape(query:string, options?:{ today?:boolean, tomorrow?:boolean, thisWeek?:boolean, thisWeekend?:boolean, nextWeek?:boolean, nextMonth?:boolean}):Promise<any> {
@@ -246,3 +246,5 @@ export default class gEventsScraper {
         return this.data;
     }
 }
+
+module.exports = gEventsScraper;
